7a9840d4c94c14c9c633888f819bfdb3ab8c22f2
Fabrício Serrano <89816062+FabricioSerrano@users.noreply.github.com>
2022-11-29 20:40:38 -0300

    Adicionado arquivo de configuração
    Adicionado arquivo de configuração json
    
    Assim como módulo de acesso a ele.

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ from json import dumps
 import MODEL.conection as c
 import VIEW.Flask as Flask
 
+from CONTROLLER.config import Config
+
 if __name__ == '__main__':
 
         try:
                 configsql = c.configSql()
 
+                config = Config()
+
+                host : str = config.iplocal()
+
+                port : int = config.port()
+
+                debug : bool = config.debug()
+
                 app = Flask.tryLoginFlask(configsql["host"],configsql["user"],configsql["password"],configsql["database"])  # type: ignore
                 
-                app.run( host="192.168.0.150", port=5000,debug=True)
+                app.run( host=host, port=port,debug=debug)
 
         except ValueError as err:
 
